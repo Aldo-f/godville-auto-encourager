0d0ad9e451dd1a999d01328e9be5468b1b6b3b2a
Jan Hovancik <conta.srdr@gmail.com>
2015-07-11 20:36:03 +0200

    [0.2] adds resume and pause possibility

--- godville-auto-encourager.user.js
+++ godville-auto-encourager.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/hovancik/godville-auto-encourager
 // @description auto-encourage your superhero when getting low on health
 // @include     https://godvillegame.com/superhero
-// @version     0.1
+// @version     0.2
 // @grant       none
 // ==/UserScript==
 
@@ -21,9 +21,23 @@ function encourage() {
   document.getElementsByClassName("no_link div_link enc_link")[0].click();
 }
 
+var encourager = setInterval( checkHealth, 15000);
+
+var pauseEncourager = function() {
+  console.log("Pausing...");
+  clearInterval(encourager);
+  encourager = 0;
+};
+
+var resumeEncourager = function() {
+  pauseEncourager();
+  console.log("Resuming...");
+  encourager = setInterval( checkHealth, 15000);
+};
+
 var divx = document.createElement('div');
-divx.innerHTML ='<div class="block" id="auto-encourage"><div class="block_h"><span class="l_slot"><span class="b_handle m_hover" style="display: none;" title="Drag to move this block">●</span></span><h2 class="block_title">Auto-Encourager</h2><span class="r_slot"><span class="h_min m_hover" style="display: none;">↑</span></span></div><div class="block_content"><p>I will check for health every 15 seconds and if health is less then 10% I will try to encourage your superhero.</div><div class="line"/></div></div>';
+divx.innerHTML ='<div class="block" id="auto-encourage"><div class="block_h"><span class="l_slot"><span class="b_handle m_hover" style="display: none;" title="Drag to move this block">●</span></span><h2 class="block_title">Auto-Encourager</h2><span class="r_slot"><span class="h_min m_hover" style="display: none;">↑</span></span></div><div class="block_content"><p>I will check for health every 15 seconds and if health is less then 10% I will try to encourage your superhero.</p><div><a style="display: inline;" class="no_link enc_link div_link" id="resume" title="Resume">Resume</a><a style="display: inline;" class="no_link pun_link div_link" id="pause" title="Pause">Pause</a></div></div><div class="line"/></div></div>';
 document.getElementById("right_block").appendChild(divx);
 
-
-setInterval( checkHealth, 15000);
+document.getElementById("pause").addEventListener("click", pauseEncourager, false);
+document.getElementById("resume").addEventListener("click", resumeEncourager, false);
