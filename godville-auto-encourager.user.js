eab35b23c752114cd3206b9ef5d929da736b38d7
Aldo <aldo.fieuw@gmail.com>
2020-01-02 18:59:35 +0100

    placed some settings on top of file

--- godville-auto-encourager.user.js
+++ godville-auto-encourager.user.js
@@ -3,17 +3,23 @@
 // @namespace   https://github.com/hovancik/godville-auto-encourager
 // @description auto-encourage your superhero when getting low on health
 // @include     https://godvillegame.com/superhero
-// @version     0.2
+// @version     0.2.0.1
 // @grant       none
 // ==/UserScript==
+
+// settings
+var minHealth = 30;
+var pauseTime = 15000;
 
 function checkHealth() {
   var healthDiv = document.getElementById("hk_health");
   var healthVal = healthDiv.getElementsByClassName("l_val")[0].innerHTML;
   var health = healthVal.split(" / ")[0];
   var possibleHealth = healthVal.split(" / ")[1];
-  var health_percents = health*100/possibleHealth
-  if (health_percents < 10) {encourage();}
+  var health_percents = (health * 100) / possibleHealth;
+  if (health_percents < minHealth) {
+    encourage();
+  }
 }
 
 function encourage() {
@@ -21,7 +27,7 @@ function encourage() {
   document.getElementsByClassName("no_link div_link enc_link")[0].click();
 }
 
-var encourager = setInterval( checkHealth, 15000);
+var encourager = setInterval(checkHealth, pauseTime);
 
 var pauseEncourager = function() {
   console.log("Pausing...");
@@ -32,12 +38,19 @@ var pauseEncourager = function() {
 var resumeEncourager = function() {
   pauseEncourager();
   console.log("Resuming...");
-  encourager = setInterval( checkHealth, 15000);
+  encourager = setInterval(checkHealth, 15000);
 };
 
-var divx = document.createElement('div');
-divx.innerHTML ='<div class="block" id="auto-encourage"><div class="block_h"><span class="l_slot"><span class="b_handle m_hover" style="display: none;" title="Drag to move this block">●</span></span><h2 class="block_title">Auto-Encourager</h2><span class="r_slot"><span class="h_min m_hover" style="display: none;">↑</span></span></div><div class="block_content"><p>I will check for health every 15 seconds and if health is less then 10% I will try to encourage your superhero.</p><div><a style="display: inline;" class="no_link enc_link div_link" id="resume" title="Resume">Resume</a><a style="display: inline;" class="no_link pun_link div_link" id="pause" title="Pause">Pause</a></div></div><div class="line"/></div></div>';
+var divx = document.createElement("div");
+divx.innerHTML =
+  '<div class="block" id="auto-encourage"><div class="block_h"><span class="l_slot"><span class="b_handle m_hover" style="display: none;" title="Drag to move this block">●</span></span><h2 class="block_title">Auto-Encourager</h2><span class="r_slot"><span class="h_min m_hover" style="display: none;">↑</span></span></div><div class="block_content"><p>I will check for health every 15 seconds and if health is less then ' +
+  minHealth +
+  '% I will try to encourage your superhero.</p><div><a style="display: inline;" class="no_link enc_link div_link" id="resume" title="Resume">Resume</a><a style="display: inline;" class="no_link pun_link div_link" id="pause" title="Pause">Pause</a></div></div><div class="line"/></div></div>';
 document.getElementById("right_block").appendChild(divx);
 
-document.getElementById("pause").addEventListener("click", pauseEncourager, false);
-document.getElementById("resume").addEventListener("click", resumeEncourager, false);
+document
+  .getElementById("pause")
+  .addEventListener("click", pauseEncourager, false);
+document
+  .getElementById("resume")
+  .addEventListener("click", resumeEncourager, false);
